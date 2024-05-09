edddbb08e22b7ee654c91cba4c92c3f845e2e11b
sebashcvo <sebastianc@cetys.edu.mx>
2024-05-09 02:05:20 -0700

    9 may

--- Arduino Code/Arduino_master_code/Arduino_master_code.ino
+++ Arduino Code/Arduino_master_code/Arduino_master_code.ino
@@ -1,15 +1,22 @@
 #include <SoftwareSerial.h>
 
 SoftwareSerial master(0,1);
-int valx=-180.27;
-
+float valx=96.27;
+float valy=100.15;
+float valz=-80.96;
 void setup() {
 Serial.begin(38400);
 master.begin(38400);
 }
 void loop() {
-  String letter="X\n";
-  String msg=valx+letter;
-  Serial.print(msg);
+  String letterx="X\n";
+  String lettery="Y\n";
+  String letterz="Z\n";
+  String msgx=valx+letterx;
+  String msgy=valy+lettery;
+  String msgz=valz+letterz;
+  Serial.print(msgx);
+  Serial.print(msgy);
+  Serial.print(msgz);
   delay(100);
 }
--- Arduino Code/Arduino_slave_code/Arduino_slave_code.ino
+++ Arduino Code/Arduino_slave_code/Arduino_slave_code.ino
@@ -1,8 +1,9 @@
 #include <SoftwareSerial.h>
 
 SoftwareSerial slave(0,1);
-String val="";
-int valx=0;
+float valx=0; //variable para almacenar angulo en X
+float valy=0; //variable para almacenar angulo en Y
+float valz=0; //variable para almacenar angulo en Z
 String msg="";
 
 
@@ -12,15 +13,23 @@ slave.begin(38400);
 }
 void loop() {
  
-while(slave.available()>0){
-  msg = Serial.readStringUntil('\n');
-  int axis=msg.length()-1;
-  String s_angulox = msg.substring(0,axis);
-  int angulox = s_angulox.toInt();
-  String completter=msg.substring(axis);
-  if(completter=="X"){
-    valx=angulox;
+while(slave.available()>0){   //Condicion en donde solo se accede a Serial cuando hay dato disponible
+  msg = Serial.readStringUntil('\n');   //El string dentro del Serial solo se lee hasta tener salto de linea
+  int axis=msg.length()-1;  //Se resta uno para poder acceder a la posicion del index del ultimo caracter
+  String s_angulo = msg.substring(0,axis); //Se extrae los caracteres que tienen un numero en el String
+  float angulo = s_angulo.toFloat();  //Se convierte el substring anterior a una variable tipo Float
+  String completter=msg.substring(axis); //Guarda el lable de letra del string original 
+  if(completter=="X"){  //Si el String es X, se guarda una variable para guardar angulo en X y se imprime en el Serial
+    valx=angulo;
     Serial.println(valx);
+  }
+  if(completter=="Y"){ //Si el String es Y, se guarda una variable para guardar angulo en Y y se imprime en el Serial
+    valy=angulo;
+    Serial.println(valy);
+  }
+  if(completter=="Z"){
+    valz=angulo;
+    Serial.println(valz); //Si el String es z, se guarda una variable para guardar angulo en Z y se imprime en el Serial
   }
   
   }
