3eac951fc3468779db385f6040e1d7f1d9fdded3
sebashcvo <sebastianc@cetys.edu.mx>
2024-05-09 00:12:56 -0700

    Functional Master/Slave Communication
    ...

--- Arduino Code/Arduino_master_code/Arduino_master_code.ino
+++ Arduino Code/Arduino_master_code/Arduino_master_code.ino
@@ -1,13 +1,15 @@
 #include <SoftwareSerial.h>
 
 SoftwareSerial master(0,1);
+int valx=-180.27;
 
 void setup() {
 Serial.begin(38400);
 master.begin(38400);
 }
 void loop() {
-  String val= "X180";
-  Serial.println(val);
+  String letter="X\n";
+  String msg=valx+letter;
+  Serial.print(msg);
   delay(100);
 }
--- Arduino Code/Arduino_slave_code/Arduino_slave_code.ino
+++ Arduino Code/Arduino_slave_code/Arduino_slave_code.ino
@@ -2,7 +2,8 @@
 
 SoftwareSerial slave(0,1);
 String val="";
-String valx="";
+int valx=0;
+String msg="";
 
 
 void setup() {
@@ -12,11 +13,16 @@ slave.begin(38400);
 void loop() {
  
 while(slave.available()>0){
-  char c=slave.read();
-  val +=c;
+  msg = Serial.readStringUntil('\n');
+  int axis=msg.length()-1;
+  String s_angulox = msg.substring(0,axis);
+  int angulox = s_angulox.toInt();
+  String completter=msg.substring(axis);
+  if(completter=="X"){
+    valx=angulox;
+    Serial.println(valx);
+  }
   
-  delay(10);
-}
-
-Serial.println(val);
-}
+  }
+  
+  }
