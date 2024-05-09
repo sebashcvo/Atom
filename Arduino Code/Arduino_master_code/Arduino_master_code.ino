fc61533d29562d1f3a58e49764455e9f6c0bfc1b
sebashcvo <sebastianc@cetys.edu.mx>
2024-05-09 02:52:04 -0700

    fILES

--- Arduino Code/Arduino_master_code/Arduino_master_code.ino
+++ Arduino Code/Arduino_master_code/Arduino_master_code.ino
@@ -1,7 +1,7 @@
 #include <SoftwareSerial.h>
 
 SoftwareSerial master(0,1);
-float valx=96.27;
+float valx=96.27; //input de datos proveniente del sensor mpu
 float valy=100.15;
 float valz=-80.96;
 void setup() {
@@ -9,14 +9,14 @@ Serial.begin(38400);
 master.begin(38400);
 }
 void loop() {
-  String letterx="X\n";
+  String letterx="X\n"; //String con funcion de Label
   String lettery="Y\n";
   String letterz="Z\n";
-  String msgx=valx+letterx;
+  String msgx=valx+letterx; //Valor Input con el label 
   String msgy=valy+lettery;
   String msgz=valz+letterz;
-  Serial.print(msgx);
+  Serial.print(msgx); //Print en el Serial
   Serial.print(msgy);
   Serial.print(msgz);
-  delay(100);
+  delay(10);
 }
